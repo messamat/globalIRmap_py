afa70ef8fe5cd3637c27f1ea6a3ed99765735fef
messamat <messamat@uw.edu>
2020-05-07 07:51:26 -0400

    small update

--- download_data.py
+++ download_data.py
@@ -24,8 +24,8 @@ from owslib.wcs import WebCoverageService  # OWSlib module to access WMS service
 
 #Folder structure
 rootdir = os.path.dirname(os.path.abspath(__file__)).split('\\globalIRmap')[0]
-datdir = os.path.join(rootdir, 'HydroATLAS', 'data')
-resdir = os.path.join(rootdir, 'HydroATLAS' , 'results')
+datdir = os.path.join(rootdir, '/HydroATLAS', 'data')
+resdir = os.path.join(rootdir, '/HydroATLAS' , 'results')
 glad_dir = os.path.join(datdir, 'GLAD')
 pathcheckcreate(glad_dir)
 
@@ -208,17 +208,17 @@ arcpy.MosaicToNewRaster_management(hysogoutlist, output_location=hysogresgdb,
 
 
 # ----------------------------- Download GLAD data ----------------------------------------------------------------------
-glad_dtype = "class99_18.tif"
+glad_dtype = "class99_19.tif"
 gsutil_ls_cmd = "gsutil ls gs://earthenginepartners-hansen/water/*/{}".format(glad_dtype)
 glad_cloudout = subprocess.check_output(gsutil_ls_cmd)
 glad_cloudlist = glad_cloudout.split('\n')
 
 for tile in glad_cloudlist:
+    tileroot = os.path.split(os.path.split(tile)[0])[1]
     out_tilen = os.path.join(glad_dir, '{0}_{1}.tif'.format(
         os.path.splitext(glad_dtype)[0], tileroot))
     if not os.path.isfile(out_tilen):
         print(tile)
-        tileroot = os.path.split(os.path.split(tile)[0])[1]
         gsutil_cp_cmd = "gsutil cp {0} {1}".format(tile, glad_dir)
         subprocess.check_output(gsutil_cp_cmd)
         os.rename(os.path.join(glad_dir, glad_dtype), out_tilen)
