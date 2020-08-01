0b158dadbb901e315c8b65acd60e73157b6ae4eb
messamat <messamat@uw.edu>
2020-08-01 05:16:27 -0400

    Start formatting GSIM stations

--- format_stations.py
+++ format_stations.py
@@ -1,8 +1,9 @@
-#Purpose: Format GRDC station data and associated environmental variables extract from HydroATLAS
+#Purpose: Format station data and associated environmental variables extract from HydroATLAS
 
 import arcpy
 import os
 import re
+from utility_functions import *
 
 arcpy.env.overwriteOutput = True
 
@@ -17,45 +18,139 @@ arcpy.env.overwriteOutput = 'True'
 arcpy.env.qualifiedFieldNames = 'False'
 
 #Input variables
-GRDCstations = os.path.join(datdir, 'GRDC_curated', 'high_qual_daily_stations.csv')
 riveratlas = os.path.join(datdir, 'HydroATLAS', 'RiverATLAS_v10.gdb', 'RiverATLAS_v10')
+riveratlasv11 = os.path.join(datdir, 'HydroATLAS', 'RiverATLAS_v11.gdb', 'RiverATLAS_v11')
 hydromask = os.path.join(datdir, 'HydroATLAS', 'Masks_20200525','hydrosheds_landmask_15s.gdb', 'hys_land_15s')
 
 #Output variables
 outgdb = os.path.join(resdir, 'spatialoutputs.gdb')
 if not arcpy.Exists(outgdb):
     arcpy.CreateFileGDB_management(os.path.split(outgdb)[0], os.path.split(outgdb)[1])
-GRDCp = os.path.join(outgdb, 'GRDCstations')
-GRDCpjoin = os.path.join(outgdb, 'GRDCstations_riverjoin')
 riveratlas_csv = os.path.join(resdir, 'RiverATLAS_v10tab.csv')
-GRDCp_aeqd = os.path.join(outgdb, "GRDCstations_aeqd")
-GRDCbuf = os.path.join(outgdb, 'GRDCstations_buf50k')
-gaugebufdiss = os.path.join(outgdb, GRDCbuf + 'diss')
+riveratlasv11_csv = os.path.join(resdir, 'RiverATLAS_v11tab.csv')
 
-#Create points for GRDC stations
-stations_coords = {row[0]:[row[1], row[2]]
-                   for row in arcpy.da.SearchCursor(GRDCstations, ['GRDC_NO', 'LONG_NEW', 'LAT_NEW'])}
-arcpy.CreateFeatureclass_management(os.path.split(GRDCp)[0], os.path.split(GRDCp)[1],
-                                    geometry_type='POINT', spatial_reference=wgs84)
-arcpy.AddField_management(GRDCp, 'GRDC_NO', 'TEXT')
+#grdc stations
+grdcstations = os.path.join(datdir, 'grdc_curated', 'high_qual_daily_stations.csv')
+grdcp = os.path.join(outgdb, 'grdcstations')
+grdcpjoin = os.path.join(outgdb, 'grdcstations_riverjoin')
+grdcp_aeqd = os.path.join(outgdb, "grdcstations_aeqd")
+grdcbuf = os.path.join(outgdb, 'grdcstations_buf50k')
+gaugebufdiss = os.path.join(outgdb, grdcbuf + 'diss')
 
-with arcpy.da.InsertCursor(GRDCp, ['GRDC_NO', 'SHAPE@XY']) as cursor:
-    for k, v in stations_coords.items():
-        cursor.insertRow([k, arcpy.Point(v[0], v[1])])
+#GSIM stations
+gsimdatdir = os.path.join(datdir, 'GSIM')
+gsimmeta = os.path.join(gsimdatdir, 'GSIM_metadata', 'GSIM_catalog', 'GSIM_metadata.csv')
+gsimsuspect = os.path.join(gsimdatdir, 'GSIM_metadata', 'GSIM_catalog', 'GSIM_suspect_coordinates_stations.csv')
+gsiminddir = os.path.join(gsimdatdir, 'GSIM_indices', 'TIMESERIES', 'monthly')
 
-#Join GRDC stations to nearest river reach in RiverAtlas
-arcpy.SpatialJoin_analysis(GRDCp, riveratlas, GRDCpjoin, join_operation='JOIN_ONE_TO_ONE', join_type="KEEP_COMMON",
-                           match_option='CLOSEST_GEODESIC', search_radius=0.0005,
-                           distance_field_name='station_river_distance')
+gsimresdir = os.path.join(resdir, 'GSIM')
+gsimresgdb = os.path.join(gsimresdir, 'GSIM.gdb')
+pathcheckcreate(gsimresgdb)
+gsimmeta_format = os.path.join(gsimresgdb, 'GSIM_metadata_format')
+gsimpraw = os.path.join(gsimresgdb, 'GSIMstations_raw')
+gsimpmeta = os.path.join(gsimresgdb, 'GSIMstations_metadata')
+
+
+######################################## FORMAT GRDC STATIONS ##########################################################
+#Create points for grdc stations
+if not arcpy.Exists(grdcp):
+    print('Create points for grdc stations')
+    stations_coords = {row[0]:[row[1], row[2]]
+                       for row in arcpy.da.SearchCursor(grdcstations, ['GRDC_NO', 'LONG_NEW', 'LAT_NEW'])}
+    arcpy.CreateFeatureclass_management(os.path.split(grdcp)[0], os.path.split(grdcp)[1],
+                                        geometry_type='POINT', spatial_reference=wgs84)
+    arcpy.AddField_management(grdcp, 'GRDC_NO', 'TEXT')
+
+    with arcpy.da.InsertCursor(grdcp, ['GRDC_NO', 'SHAPE@XY']) as cursor:
+        for k, v in stations_coords.items():
+            cursor.insertRow([k, arcpy.Point(v[0], v[1])])
+
+#Join grdc stations to nearest river reach in RiverAtlas
+if not arcpy.Exists(grdcpjoin):
+    print('Join grdc stations to nearest river reach in RiverAtlas')
+    arcpy.SpatialJoin_analysis(grdcp, riveratlas, grdcpjoin, join_operation='JOIN_ONE_TO_ONE', join_type="KEEP_COMMON",
+                               match_option='CLOSEST_GEODESIC', search_radius=0.0005,
+                               distance_field_name='station_river_distance')
 
 #Export attribute table of RiverATLAS with selected
-arcpy.CopyRows_management(in_rows = riveratlas, out_table=riveratlas_csv)
+if not arcpy.Exists(riveratlas_csv):
+    arcpy.CopyRows_management(in_rows = riveratlas, out_table=riveratlas_csv)
+if not arcpy.Exists(riveratlasv11_csv):
+    arcpy.CopyRows_management(in_rows=riveratlasv11, out_table=riveratlasv11_csv)
+
+######################################## FORMAT GSIM STATIONS ##########################################################
+#Replace points with underscores in metadata table and import into geodatabase with correct field types
+if not arcpy.Exists(gsimmeta_format):
+    metarawtab = pd.read_csv(gsimmeta)
+    metarawtab.columns = [re.sub('[.]', '_', s) for s in metarawtab.columns]
+    x = metarawtab.reset_index()
+    z = np.rec.fromrecords(x, names=x.columns.tolist())
+    arcpy.da.NumPyArrayToTable(z, gsimmeta_format)
+
+#Create points for GSIM stations
+if not arcpy.Exists(gsimpmeta):
+    arcpy.MakeXYEventLayer_management(gsimmeta_format, in_x_field='longitude', in_y_field='latitude',
+                                      out_layer='gsimpmetatest', spatial_reference=wgs84)
+    arcpy.SaveToLayerFile_management('gsimpmetatest', os.path.join(gsimresgdb, 'test'))
+    arcpy.CopyFeatures_management(os.path.join(gsimresgdb, 'test.lyr'), gsimpmeta)
+
+#Only keep those with drainage area information and at least 10 years of data (with max 20 years of missing data per year)
+gsimpsub = os.path.join(gsimresgdb, 'GSIMSstations_sub')
+if not arcpy.Exists(gsimpsub):
+    arcpy.MakeFeatureLayer_management(gsimpmeta, 'gsimpmetalyr',  where_clause= '(area > 0) AND (number_available_days > 3450)')
+    arcpy.CopyFeatures_management('gsimpmetalyr', gsimpsub)
+
+    #Delete those that are already in the curated GRDC database or with suspect coordinates
+    grdcids = [row[0] for row in arcpy.da.SearchCursor(grdcp, 'GRDC_NO')]
+    suspectids= pd.read_csv(gsimsuspect)['reference.no'].to_list()
+    x=0
+    with arcpy.da.UpdateCursor(gsimpsub, ['grdb_no', 'reference_no']) as cursor:
+        for row in cursor:
+            if row[0] is not None:
+                if str(int(row[0])) in grdcids:
+                    cursor.deleteRow()
+                    x+=1
+                    continue
+            if row[1] in suspectids:
+                cursor.deleteRow()
+                x += 1
+    print('Deleted {} gauges in GSIM that were already in the curated GRDC datasets or had suspect coordinates'.format(x))
+    del x
+    del grdcids
+
+#Merge all monthly discharge indices time series for gauges in the initial selection
+gsimsubno = [row[0] for row in arcpy.da.SearchCursor(gsimpsub, 'gsim_no')]
+def format_gsimmontab(tab, filterlist):
+    tabno = os.path.splitext(os.path.split(tab)[1])[0]
+    print(tabno)
+    if tabno in filterlist:
+        tsdf = pd.read_csv(tab, sep=',\\t', header=21)
+        tsdf.columns = [re.sub('"', '', s) for s in tsdf.columns]
+        tsdf['gsim_no'] = tabno
+        return(tsdf)
+
+gsimsub_modf = pd.concat([format_gsimmontab(file, filterlist=gsimsubno) for file in getfilelist(gsiminddir)],
+                         axis=0) \
+    .sort_index()
+
+gsimsub_modf['year'] = gsimsub_modf['date']
+gsimsub_modf['month'] =
+gsimsub_modf['day'] =
+
+#Only keep those that have at least 10 years of data, only counting years with less than 20 days of missing data,
+#and that have average discharge of at least 0.01 m3/s OR a reported drainage area >= 9 km2
+
+
+
+
+#Spatial Join to HydroATLAS
+
 
 #------------------------------- Create grid for prediction error mapping ----------------------------------------------
 #Buffer gauging stations
-arcpy.Buffer_analysis(GRDCp, GRDCbuf, buffer_distance_or_field='50 kilometers',
+arcpy.Buffer_analysis(grdcp, grdcbuf, buffer_distance_or_field='50 kilometers',
                       dissolve_option='ALL', method='GEODESIC')
-arcpy.Dissolve_management(GRDCbuf, gaugebufdiss, multi_part='FALSE')
+arcpy.Dissolve_management(grdcbuf, gaugebufdiss, multi_part='FALSE')
 
 #Split
 bufgdb = os.path.join(resdir, 'gaugebuf.gdb')
@@ -111,21 +206,21 @@ for cont in hydrodir_list.keys():
 
     if not arcpy.Exists(bufmask_dict[cont]):
         print('Processing {}...'.format(bufmask_dict[cont]))
-        arcpy.MakeFeatureLayer_management(GRDCp, 'grdcp')
+        arcpy.MakeFeatureLayer_management(grdcp, 'grdcp')
         arcpy.SelectLayerByLocation_management('grdcp', overlap_type='INTERSECT', select_features=tiledesc.extent.polygon)
         arcpy.CopyFeatures_management('grdcp', bufmask_dict[cont])
 
-    arcpy.Buffer_analysis(bufmask_dict[cont], GRDCbuf, buffer_distance_or_field= '50 kilometers',
+    arcpy.Buffer_analysis(bufmask_dict[cont], grdcbuf, buffer_distance_or_field= '50 kilometers',
                           dissolve_option='ALL', method='GEODESIC')
     #Tile by continent
     arcpy.env.snapRaster = hydromask
     arcpy.env.cellSize = hydromask
-    outbufras = os.path.join(resdir, 'GRDCstations_bufras{}.tif'.format(cont))
+    outbufras = os.path.join(resdir, 'grdcstations_bufras{}.tif'.format(cont))
     print('Processing {}...'.format(outbufras))
-    arcpy.PolygonToRaster_conversion(GRDCbuf, value_field='OBJECTID', out_rasterdataset=outbufras,
+    arcpy.PolygonToRaster_conversion(grdcbuf, value_field='OBJECTID', out_rasterdataset=outbufras,
                                      cellsize=arcpy.Describe(hydromask).meanCellWidth)
 
-    outbufrasproj = os.path.join(resdir, 'GRDCstations_bufrasproj{}.tif'.format(cont))
+    outbufrasproj = os.path.join(resdir, 'grdcstations_bufrasproj{}.tif'.format(cont))
     print('Processing {}...'.format(outbufrasproj))
     arcpy.ProjectRaster_management(outbufras, outbufrasproj,
                                    out_coor_system= arcpy.SpatialReference(54032), resampling_type='NEAREST',
